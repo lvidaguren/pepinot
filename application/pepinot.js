9d99e204c5bef822d6fdb37266869a092a334a74
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2015-06-11 20:10:33 -0300

    Se agregó tipo hablar y hay pruebas de mostrar tipo y hablar separamos los estilos en main.css

--- application/pepinot.js
+++ application/pepinot.js
@@ -1,4 +1,60 @@
 $(function() {
 	//$("#intro").addClass("rojo");
 	//$("#intro span").addClass("grande");
+	//presentarTipo(nombreTipoPrincipal);
+	//presentarTipo(nombreTipo2);
+	
+	var feli = crearTipo("Felipe");
+	
+	$("body").append(feli);
+	
+	$("body").click(function(){
+		var p = pensar("Esto es buenisimo!!!!!!!");
+		p.addClass('rojo');
+		$("body").append(p);
+		p.show();
+		
+		$(".tipo").toggle();
+	})
 });
+
+var nombreTipoPrincipal = "Pepinot";
+var nombreTipo2 = "Tonipep";
+
+function crearTipo(nombre) {
+	var todoElTipo = $("<div class='tipo'/>");
+	var cabeza = $("<div class='head' />");
+	var cuerpo = $("<div class='body' />");
+	var brazoIzq = $("<div class='arm left' />");
+	var brazoDer = $("<div class='arm right' />");
+	
+	var piernaIzq = $("<div class='leg left' />");
+	var piernaDer = $("<div class='leg right' />");
+	
+	todoElTipo.append(cabeza);
+	todoElTipo.append(cuerpo);
+	todoElTipo.append(brazoIzq);
+	todoElTipo.append(brazoDer);
+	todoElTipo.append(piernaIzq);
+	todoElTipo.append(piernaDer);
+	todoElTipo.attr('id',nombre);
+	
+	return todoElTipo;
+}
+
+//function presentarTipo(queTipo) {
+//	var divTipo = $("<div class='tipo' />").append(queTipo);
+//	divTipo.addClass("grande");
+//	$("body").append(divTipo);
+//	divTipo.show("slow");
+//}
+
+function hablar(palabras) {
+	var pensamiento = pensar(palabras);
+	$("body").append(pensamiento);
+	pensamiento.show();
+}
+
+function pensar(palabras) {
+	return $("<div class='globoTexto'>"+palabras+"</div>");
+}
